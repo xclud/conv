7f597aff59bb30c9408f23f14b079fc7a6ef4173
Mahdi <mkfard@outlook.com>
2022-03-24 21:14:51 +0430

    Skip SSL.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+curl --insecure -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet --version
